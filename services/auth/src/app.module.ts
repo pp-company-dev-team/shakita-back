7dbb2f5669f34e8473113c25dfa50ed117c09ac8
alex01aik <alexashka010100@gmail.com>
2023-05-13 15:56:04 +0200

    postgres

--- services/auth/src/app.module.ts
+++ services/auth/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'shakita',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'shakita',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
