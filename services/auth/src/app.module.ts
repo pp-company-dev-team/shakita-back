445d08217bdf077d3a3b113e1974332afe5eec49
Alex01Aik <52621233+Alex01aik@users.noreply.github.com>
2023-05-15 23:26:25 +0200

    Revert "test for merge"

--- services/auth/src/app.module.ts
+++ services/auth/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { Test } from './test.entity';
-// test for merge
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
